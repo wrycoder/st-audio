ec952ef7c990d91accbdeddf8ac68a4b43ff3f23
Michael Toulouse <wrycoder@ameritech.net>
2023-06-20 11:56:09 +0000

    Adding global functions and defaults

--- splice.h
+++ splice.h
@@ -26,12 +26,17 @@
 
 #define SOX_LIB_ERROR 399
 #define linear_to_dB(x) (log10(x) * 20)
-#define DEFAULT_SILENCE_THRESHOLD ".017"
+#define DEFAULT_SILENCE_THRESHOLD ".041"
+#define DEFAULT_NOISE_DURATION "00:00:00.2"
+#define DEFAULT_OUTPUT_FILENAME "spliced-audio.wav"
+#define MAXIMUM_SPLICES 50
 
 void show_stats(sox_format_t * in);
 void show_name_and_runtime(sox_format_t * in);
 wchar_t const * str_time(double seconds);
 void report_error(HWND hwnd, int errcode, int line_number);
 const char* ConvertPWSTRToConstChar(PWSTR wideString);
-void trim_silence(wchar_t * filename, char * threshold);
+void trim_silence(wchar_t * filename, char * duration, char * threshold);
+void splice();
+static int sox_quit_called;
 int cleanup();
